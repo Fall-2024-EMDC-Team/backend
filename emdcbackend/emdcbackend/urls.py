a73ef121a801c0380e6d112e44fdc2a2084e5a1d
eMack27 <ethan@macksam.net>
2024-09-11 10:50:43 -0500

    Temporarily Commented Out Redirect To Catalog

--- emdcbackend/emdcbackend/urls.py
+++ emdcbackend/emdcbackend/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/', permanent=True)),  # Add URL maps to redirect the base URL to our application
+    # path('', RedirectView.as_view(url='catalog/', permanent=True)),  # Add URL maps to redirect the base URL to our application
     static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),  # Use static() to add URL mapping to serve static files during development (only)
 ]
