f4d5c2e0318f996a45d5c46199437aade90cbe11
Ethan M <ethan@macksam.net>
2024-09-11 14:02:05 -0500

    Cleaned Up Repo, Got Tutorial REST API Working

--- emdcbackend/emdcbackend/asgi.py
+++ emdcbackend/emdcbackend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emdcbackend.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emdcbackend.settings')
 
 application = get_asgi_application()
--- emdcbackend/emdcbackend/urls.py
+++ emdcbackend/emdcbackend/urls.py
@@ -17,17 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from emdcbackend.emdcbackend import views
+from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
+# Because we're using viewsets instead of views, we can automatically generate the URL conf for our API,
+# by simply registering the viewsets with a router class.
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-# admin user password temporarily set to: emdc1234
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
