a420acdac6c970c8ee4e75bf701302b0cae12eec
eMack27 <ethan@macksam.net>
2024-09-24 12:51:41 -0500

    Modified API Endpoints and User Serializer
    Django Auth requires both a username and password fields in order to operate. We're going to store the email addresses in the username field.
    
    Added a userType field to the UserSerializer.

--- emdcbackend/emdcbackend/auth/serializers.py
+++ emdcbackend/emdcbackend/auth/serializers.py
@@ -4,4 +4,5 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
-        fields = ['id', 'email', 'password']
+        fields = ['id', 'username', 'password', 'userType']  # Django auth requires at least a username and password field
+                                                             # Use username field for email addresses
--- emdcbackend/emdcbackend/auth/views.py
+++ emdcbackend/emdcbackend/auth/views.py
@@ -8,29 +8,36 @@ from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
 from django.shortcuts import get_object_or_404
 
+# login endpoint
 @api_view(['POST'])
 def login(request):
-    user = get_object_or_404(User, email=request.data['email'])   # check if user's email exists
+    user = get_object_or_404(User, username=request.data['username'])   # check if user's email exists
     if not user.check_password(request.data['password']):   # if password is incorrect
         return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
     token, created = Token.objects.get_or_create(user=user)
     serializer = UserSerializer(instance=user)
     return Response({"token": token.key, "user": serializer.data})
 
+# signup endpoint
 @api_view(['POST'])
 def signup(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()   # add user to DB
-        user = User.objects.get(email=request.data['email'])
+        user = User.objects.get(username=request.data['username'])
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
         return Response({"token": token.key, "user": serializer.data})
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # If data is bad
 
+# delete user API
+
+# edit user (password change, email change) API
+
+# token verification endpoint
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def test_token(request):
-    return Response({"passed for {}".format(request.user.email)})
+    return Response({"passed for {}".format(request.user.username)})
