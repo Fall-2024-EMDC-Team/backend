0c5871f779a2a7dc3dac0e61d38df421ea7db158
bodiebice <bodiebice@gmail.com>
2024-10-15 09:22:20 -0500

    cluster stash

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 migrations:
 
 python manage.py makemigrations
-
 python manage.py migrate
 
 python manage.py makemigrations auth
--- emdcbackend/emdcbackend/views/clusters.py
+++ emdcbackend/emdcbackend/views/clusters.py
@@ -12,7 +12,7 @@ from django.shortcuts import get_object_or_404
 from django.db import transaction
 from ..models import JudgeClusters
 from ..serializers import JudgeClustersSerializer
-from .Maps.MapClusterToContest import  create_cluster_contest_mapping
+from .Maps.MapClusterToContest import  map_cluster_to_contest
 
 @api_view(["GET"])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
@@ -36,20 +36,21 @@ def clusters_get_all(request):
 def create_cluster(request):
     try:
         with transaction.atomic():
+          
             if JudgeClusters.objects.filter(cluster_name=request.data["cluster_name"]).exists():
                 return Response(
                     {"detail": "A cluster with this name already exists."},
                     status=status.HTTP_400_BAD_REQUEST
                 )
             
-            serializer = JudgeClustersSerializer(data=request.data)
+            cluster_response = create_cluster(request.data)
             if serializer.is_valid():
                 serializer.save()
                 # map team to cluster
                 responses = [
-                    create_cluster_contest_mapping({
+                    map_cluster_to_contest({
                     "contestid": request.data["contestid"],
-                    "clusterid": request.data["clusterid"]
+                    "clusterid": cluster_response.get("id")
                     })
                 ]
                 # Check for any errors in mapping responses
@@ -57,11 +58,10 @@ def create_cluster(request):
                     if isinstance(response, Response):
                         return response
 
-                return Response({"cluster": serializer.data},status=status.HTTP_200_OK)
-            
-            return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
+                return Response({
+                  "cluster": cluster_response,
+                  "cluster to contest map:":responses[0]
+                },status=status.HTTP_201_CREATED)
 
     except ValidationError as e:  # Catching ValidationErrors specifically
         return Response({"errors": e.detail}, status=status.HTTP_400_BAD_REQUEST)
@@ -69,21 +69,6 @@ def create_cluster(request):
     except Exception as e:
         return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-# @api_view(["POST"])  # map judge and team to cluster
-# @authentication_classes([SessionAuthentication, TokenAuthentication])
-# @permission_classes([IsAuthenticated])
-# def create_cluster(request):
-   
-#    responses = [
-#          create_cluster_team_mapping({
-#             "teamid": request.data["teamid"],
-#             "clusterid": request.data["clusterid"]
-#          })
-#    ]
-#     # Check for any errors in mapping responses
-#     for response in responses:
-#         if isinstance(response, Response):
-#             return response
 
 @api_view(["POST"])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
@@ -110,3 +95,18 @@ def delete_cluster(request, cluster_id):
     cluster = get_object_or_404(JudgeClusters, id=cluster_id)
     cluster.delete()
     return Response({"detail": "Cluster deleted successfully."}, status=status.HTTP_200_OK)
+
+def make_judge_cluster_instance(data):
+  serializer = JudgeClustersSerializer(data=data)
+  if serializer.is_valid():
+      serializer.save()
+      return serializer.data
+  raise ValidationError(serializer.errors)
+
+
+def make_cluster(data):
+  cluster_data = {"cluster_name":data["cluster_name"]}
+  cluster_response = make_judge_cluster_instance(cluster_data)
+  if not cluster_response.get('id'):
+        raise ValidationError('Cluster creation failed.')
+  return cluster_response
